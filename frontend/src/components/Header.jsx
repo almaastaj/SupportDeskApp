b762401140037685a116b63eb4c1f6c1820cd842
Almaas Taj <almaastaj123@gmail.com>
2024-02-14 23:23:01 +0530

    Add Header Component and initial Pages

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Support Desk Logo" style={{ height: "32px", alignContent: "center" }} />
+                <span> </span>
+                <Link style={{ fontSize: "28px", fontWeight: "bold" }} to="/">
+                    Support Desk
+                </Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to="/login">
+                        <FaSignInAlt />
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/register">
+                        <FaUser />
+                        Register
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
